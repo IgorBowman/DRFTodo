e2db7f3b85cc68642297bec89729d24d019ec57b
IgorBowman <jitser26@gmail.com>
2022-09-06 21:25:31 +0400

    [add] created TodoListApiView post method

--- todo/todo_api/views.py
+++ todo/todo_api/views.py
@@ -14,3 +14,16 @@ class TodoListApiView(APIView):
         todos = Todo.objects.filter(user=request.user.id)
         serializer = TodoSerializer(todos, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request, *args, **kwargs):
+        data = {
+            'task': request.data.get('task'),
+            'completed': request.data.get('completed'),
+            'user': request.user.id
+        }
+        serializer = TodoSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
