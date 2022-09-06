da81c1472b6425d0c0481a1eacac13a37fef80d9
IgorBowman <jitser26@gmail.com>
2022-09-06 21:21:19 +0400

    [add] created TodoListApiView get method

--- todo/todo_api/views.py
+++ todo/todo_api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import permissions
 
-# Create your views here.
+from .models import Todo
+from .serializers import TodoSerializer
+
+
+class TodoListApiView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request, *args, **kwargs):
+        todos = Todo.objects.filter(user=request.user.id)
+        serializer = TodoSerializer(todos, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
